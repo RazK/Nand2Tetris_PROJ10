8df6d73c254aabb274dfbbdb13a20b4b290ac2b2
Noy <noystll@gmail.com>
2017-12-12 11:20:37 +0200

    regex for comments

--- JackGrammar.py
+++ JackGrammar.py
@@ -7,4 +7,8 @@
 # REGULAR EXPRESSIONS #
 #######################
 
-RE_WHITESPACES = r'\s' # https://regex101.com/r/evyXL2/1
+# COMMENTS AND WHITE SPACES:
+RE_WHITESPACES = r'\s'  # https://regex101.com/r/evyXL2/1
+RE_END_OF_LINE_COMMENT = "//.*"  # https://regex101.com/r/PbLBSc/1
+RE_IN_LINE_COMMENTS = "/\*.*?\*/"  #https://regex101.com/r/PbLBSc/2
+
