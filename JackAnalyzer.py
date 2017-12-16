36738ffd1ba3ab2b8fbe51769983224a16f28b08
Noy <noystll@gmail.com>
2017-12-16 12:42:13 +0200

    Updated main

--- JackAnalyzer.py
+++ JackAnalyzer.py
@@ -10,8 +10,8 @@
 ###############################################################################
 import os
 import sys
+from CompilationEngine import *
 from JackTokenizer import *
-import xml.etree.cElementTree as ET
 
 SOURCE_EXTENSION = ".jack"
 OUTPUT_EXTENSION = ".xml"
@@ -22,9 +22,10 @@ XML_DELIM_NON_TERMINAL = "\n"
 TOKEN_ROOT_START = "<tokens>\n"
 TOKEN_ROOT_END = "</tokens>\n"
 
+
 def main(path):
     """
-    Translate the .jack source file (or files) in the given path into a token
+    Translates the .jack source file (or files) in the given path into a
     .xml output file.
     """
 
@@ -51,58 +52,88 @@ def main(path):
     # Assemble all files
     analyze(sources)
 
-def addToken(outfile, token, token_type, isTerminal=True):
-    delimiter = XML_DELIM_TERMINAL if isTerminal else XML_DELIM_NON_TERMINAL
-    tag = "<{0}>{1}{2}{1}</{0}>\n".format(token_type, delimiter, token)
-    outfile.write(tag)
 
 def analyze(sources):
     """
     For each source Xxx.jack file, the analyzer goes through the
     following logic:
-    1.  Create a JackTokenizer from the Xxx.jack input file;
-    2.  Create an output file called Xxx.xml and prepare it for writing;
-    3.  Use the CompilationEngine to compile the input JackTokenizer into the
+    1.  Creates an output file called Xxx.xml and prepare it for writing;
+    2.  Use the CompilationEngine to compile the input JackTokenizer into the
         output file.
     :param sources: list of names of sources to compile.
     """
 
-    # Parse each source and translate to it the output
+    # Parse each source and translates to it the output:
     for sourcename in sources:
         base = os.path.splitext(sourcename)[0]
         outname = base + OUTPUT_EXTENSION
 
         # Open source for analyzing, output file for writing
         with open(sourcename, 'r') as source, open(outname, 'w') as output:
-            # Create a JackTokenizer from the Xxx.jack input file
-            tokenizer = JackTokenizer(source)
-            output.write(TOKEN_ROOT_START)
+            # Create a CompilationEngine from the Xxx.jack input file
+            engine = CompilationEngine(source, output)
+            engine.compileClass()
 
-            # Parse each command line in the source and translate
-            while (tokenizer.hasMoreTokens()):
-                # Use the CompilationEngine to compile the input
-                # JackTokenizer into the output file
-                tokenizer.advance()
-                token_type = tokenizer.tokenType()
-                if token_type == TOKEN_TYPE_KEYWORD:
-                    keyword = tokenizer.keyWord()
-                    addToken(output, keyword, TOKEN_TYPE_KEYWORD)
-                elif token_type == TOKEN_TYPE_SYMBOL:
-                    symbol = tokenizer.symbol()
-                    addToken(output, symbol, TOKEN_TYPE_SYMBOL)
-                elif token_type == TOKEN_TYPE_INTEGER:
-                    integer = tokenizer.intVal()
-                    addToken(output, integer, TOKEN_TYPE_INTEGER)
-                elif token_type == TOKEN_TYPE_STRING:
-                    string = tokenizer.stringVal()
-                    addToken(output, string, TOKEN_TYPE_STRING)
-                elif token_type == TOKEN_TYPE_IDENTIFIER:
-                    identifier = tokenizer.identifier()
-                    addToken(output, identifier, TOKEN_TYPE_IDENTIFIER)
-            output.write(TOKEN_ROOT_END)
 
 if (__name__ == "__main__"):
     if len(sys.argv) > 1:
         main(sys.argv[1])
     else:
         main(DEFAULT_SOURCE_FILE)
+
+
+
+
+        ############
+        # OLD CODE #
+        ############
+
+        # def addToken(outfile, token, token_type, isTerminal=True):
+        #     delimiter = XML_DELIM_TERMINAL if isTerminal else XML_DELIM_NON_TERMINAL
+        #     tag = "<{0}>{1}{2}{1}</{0}>\n".format(token_type, delimiter, token)
+        #     outfile.write(tag)
+        #
+        # def analyze(sources):
+        #     """
+        #     For each source Xxx.jack file, the analyzer goes through the
+        #     following logic:
+        #     1.  Create a JackTokenizer from the Xxx.jack input file;
+        #     2.  Create an output file called Xxx.xml and prepare it for writing;
+        #     3.  Use the CompilationEngine to compile the input JackTokenizer into the
+        #         output file.
+        #     :param sources: list of names of sources to compile.
+        #     """
+        #
+        #     Parse each source and translate to it the output
+        # for sourcename in sources:
+        #     base = os.path.splitext(sourcename)[0]
+        #     outname = base + OUTPUT_EXTENSION
+        #
+        #     Open source for analyzing, output file for writing
+        # with open(sourcename, 'r') as source, open(outname, 'w') as output:
+        #     Create a JackTokenizer from the Xxx.jack input file
+        # tokenizer = JackTokenizer(source)
+        # output.write(TOKEN_ROOT_START)
+        #
+        # Parse each command line in the source and translate
+        # while (tokenizer.hasMoreTokens()):
+        #     Use the CompilationEngine to compile the input
+        #     JackTokenizer into the output file
+        # tokenizer.advance()
+        # token_type = tokenizer.tokenType()
+        # if token_type == TOKEN_TYPE_KEYWORD:
+        #     keyword = tokenizer.keyWord()
+        #     addToken(output, keyword, TOKEN_TYPE_KEYWORD)
+        # elif token_type == TOKEN_TYPE_SYMBOL:
+        #     symbol = tokenizer.symbol()
+        #     addToken(output, symbol, TOKEN_TYPE_SYMBOL)
+        # elif token_type == TOKEN_TYPE_INTEGER:
+        #     integer = tokenizer.intVal()
+        #     addToken(output, integer, TOKEN_TYPE_INTEGER)
+        # elif token_type == TOKEN_TYPE_STRING:
+        #     string = tokenizer.stringVal()
+        #     addToken(output, string, TOKEN_TYPE_STRING)
+        # elif token_type == TOKEN_TYPE_IDENTIFIER:
+        #     identifier = tokenizer.identifier()
+        #     addToken(output, identifier, TOKEN_TYPE_IDENTIFIER)
+        # output.write(TOKEN_ROOT_END)
